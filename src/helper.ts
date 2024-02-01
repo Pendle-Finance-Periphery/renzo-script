4d1d66aa6255074482ac46c6a18b48d486427b57
ngfam <ngfamgithub@gmail.com>
2024-02-02 00:11:47 +0700

    Added liquid locker checker

--- src/helper.ts
+++ src/helper.ts
@@ -1,6 +1,7 @@
 import { PENDLE_POOL_ADDRESSES } from "./consts.js";
 
 export function isPendleAddress(addr: string) {
+    addr = addr.toLowerCase();
     return addr == PENDLE_POOL_ADDRESSES.SY ||
         addr == PENDLE_POOL_ADDRESSES.YT ||
         addr == PENDLE_POOL_ADDRESSES.LP;
@@ -8,7 +9,8 @@ export function isPendleAddress(addr: string) {
 
 // @TODO: to modify this when liquid lockers launch
 export function isLiquidLockerAddress(addr: string) {
-    return false; 
+    addr = addr.toLowerCase();
+    return PENDLE_POOL_ADDRESSES.LIQUID_LOCKERS.some((liquidLockerInfo) => liquidLockerInfo.address == addr);
 }
 
 export function getUnixTimestamp(date: Date) {
