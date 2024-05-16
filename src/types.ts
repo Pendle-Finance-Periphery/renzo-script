0878c4b9eed1075b1f0523298b256e24e706b9c8
ngfam <ngfamgithub@gmail.com>
2024-05-16 18:09:04 +0700

    Added position datas

--- src/types.ts
+++ src/types.ts
@@ -7,6 +7,13 @@ export const POINT_SOURCE_YT: POINT_SOURCE = "YT"
 export const POINT_SOURCE_LP: POINT_SOURCE = "LP"
 export const POINT_SOURCE_SY: POINT_SOURCE = "SY"
 
+export type UPDATE_TYPE = "market_info" | "liquid_locker_info" | "user_market_info" | "user_liquid_locker_info" | "user_yt_position"
+export const UPDATE_TYPE_MARKET_INFO: UPDATE_TYPE = "market_info"
+export const UPDATE_TYPE_LIQUID_LOCKER_INFO: UPDATE_TYPE = "liquid_locker_info"
+export const UPDATE_TYPE_USER_MARKET_INFO: UPDATE_TYPE = "user_market_info"
+export const UPDATE_TYPE_USER_LIQUID_LOCKER_INFO: UPDATE_TYPE = "user_liquid_locker_info"
+export const UPDATE_TYPE_USER_YT_POSITION: UPDATE_TYPE = "user_yt_position"
+
 export type PointAmounts = {
     ezPoint: bigint
     elPoint: bigint
