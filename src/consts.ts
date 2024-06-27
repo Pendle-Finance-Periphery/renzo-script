e168f5642230b3c5b399ab3686edbbe2e6f8db5e
ngfam <ngfamgithub@gmail.com>
2024-06-27 10:27:01 +0700

    Config for zircuit aug pool

--- src/consts.ts
+++ src/consts.ts
@@ -17,19 +17,19 @@ export const SYNCING_CONFIG = {
 
 export const PENDLE_POOL_ADDRESSES = {
     network: EthChainId.ETHEREUM,
-    SY: "0x22e12a50e3ca49fb183074235cb1db84fe4c716d",
-    YT: "0x7749f5ed1e356edc63d469c2fcac9adeb56d1c2b",
+    SY: "0x7a493be5c2ce014cd049bf178a1ac0db1b434744",
+    YT: "0x87baf4b42c075db7eb1932a0a49a5465e9a5ce9f",
     LPs: [
         {
-            address: "0xd8f12bcde578c653014f27379a6114f67f0e445f",
-            deployedBlock: 19703129            
+            address: "0xee6bdfac6767efef0879b924fea12a3437d281a2",
+            deployedBlock: 20158641            
         },
     ],
-    START_BLOCK: 19703129,
-    EXPIRY: 1735171200,
+    START_BLOCK: 20158641,
+    EXPIRY: 1724284800,
     TREASURY: "0x8270400d528c34e1596ef367eedec99080a1b592",
-    EQB_STAKING: "0x9fe99f1071b01bc04bbad0d48b59ca0e7b23e0b1",
-    PENPIE_RECEIPT_TOKEN: "0xa809f81b1d9278b7dd0eb88df64cdb474966dbe1",
+    EQB_STAKING: "0xd64c8d303ce9d0b6da9261be55e408f26c15f0d8",
+    PENPIE_RECEIPT_TOKEN: "0x75cb79a5546fafae16e968e1e81c7dcb51844674",
 
     // STAKEDAO_RECEIPT_TOKEN: "0xdd9df6a77b4a4a07875f55ce5cb6b933e52cb30a",
     MULTICALL: "0xca11bde05977b3631167028862be2a173976ca11",
@@ -37,16 +37,16 @@ export const PENDLE_POOL_ADDRESSES = {
         {
             // Penpie
             address: "0x6e799758cee75dae3d84e09d40dc416ecf713652",
-            receiptToken: "0xa809f81b1d9278b7dd0eb88df64cdb474966dbe1",
-            lpAddress: "0xd8f12bcde578c653014f27379a6114f67f0e445f",
-            deployedBlock: 19714868
+            receiptToken: "0x75cb79a5546fafae16e968e1e81c7dcb51844674",
+            lpAddress: "0xee6bdfac6767efef0879b924fea12a3437d281a2",
+            deployedBlock: 20173903
         },
         {
             // EQB
             address: '0x64627901dadb46ed7f275fd4fc87d086cff1e6e3',
-            receiptToken: "0x9fe99f1071b01bc04bbad0d48b59ca0e7b23e0b1",
-            lpAddress: "0xd8f12bcde578c653014f27379a6114f67f0e445f",
-            deployedBlock: 19716418
+            receiptToken: "0xd64c8d303ce9d0b6da9261be55e408f26c15f0d8",
+            lpAddress: "0xee6bdfac6767efef0879b924fea12a3437d281a2",
+            deployedBlock: 20173564
         },
         // {   // STAKEDAO
         //     address: '0xd8fa8dc5adec503acc5e026a98f32ca5c1fa289a',
